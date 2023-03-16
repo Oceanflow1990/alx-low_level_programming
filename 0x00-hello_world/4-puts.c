9680dcf499f8788f3c684c1ad90079ee49686a9a
oceanflow1990 <eco:ajibade@gmail.com>
2023-03-16 15:50:05 -0700

    Task 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  *  main - This program pritns a line with puts function
  *  Return: 0 (pass)
  */
-int main(void)
+int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
