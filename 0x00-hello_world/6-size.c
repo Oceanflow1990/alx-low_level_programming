2acf9ecabcf7b69a8eb405601d7c43b5bd09d165
oceanflow1990 <eco:ajibade@gmail.com>
2023-03-16 17:34:12 -0700

    Task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,13 +8,14 @@ int main(void)
 	char a;
 	int b;
 	long int c;
-	long long int d;
+	long long
+	int d;
 	float f;
 
 printf("Siz of a char: %lubyte($)\n", (unsigned long)sizeof(a));
 printf("Siz of a int: %lubyte($)\n", (unsigned long)sizeof(b));
 printf("Siz of a long int: %lubyte($)\n", (unsigned long)sizeof(c));
 printf("Siz of a long long int: %lubyte($)\n", (unsigned long)sizeof(d));
-printf("Siz of a  float: %lubyte($)\n", (unsigned long)sizeof(f));
+printf("Siz of a float: %lubyte($)\n", (unsigned long)sizeof(f));
 return (0);
 }
