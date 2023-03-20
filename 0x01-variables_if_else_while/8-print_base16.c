f3b9e79008359ab58f18745b998596b9df70ad8d
oceanflow1990 <eco:ajibade@gmail.com>
2023-03-20 02:53:03 -0700

    Task 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,20 +2,17 @@
 
 /**
  * main - prtins number between 0 to  9 and lettrs between a to f.
- * return: Always 0 (pass)
+ * Return: Always 0 (pass)
  */
 int main(void)
 {
-	int 1;
+	int i;
+	char j;
 
-	for (i = 48; i < 58; i++)
-	{
-		putchar(i);
-	}
-	for (i = 97; 1 < 103; i++)
-	{
-		putchar(i);
-	}
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
 	putchar('\n');
 	return (0);
 }
