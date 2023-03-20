32bf68b1819d3a38c2f538ed1f219d408361008d
oceanflow1990 <eco:ajibade@gmail.com>
2023-03-20 02:34:20 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * main - prtins the alphabet without q and e.
- * return always 0 (pass)
+ * main - prints the alphabet without q and e.
+ * Return: always 0 (pass)
  */
 int main(void)
 {
 	char i;
-	
-	for (i = 'a' ; <= 'z' ; i++)
+
+	for (i = 'a' ; i <= 'z' ; i++)
 		if (i != 'q' && i != 'e')
 			putchar(i);
-
 	putchar('\n');
 	return (0);
 }
