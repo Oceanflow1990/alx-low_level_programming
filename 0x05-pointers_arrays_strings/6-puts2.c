d84fe2a2c4c5abb0355f7d21802697c0be9f7329
oceanflow1990 <eco:ajibade@gmail.com>
2023-03-28 15:27:12 -0700

    Task 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,14 +8,12 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	for (; str[i] != '\0'; i++)
+	for (i = 0 ; str[i] != '\0' ; i++)
 	{
-		if ((i % 2) == 0)
-			_putchar(str[i]);
-		else
-			continue
+		_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
