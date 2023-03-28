c067002adb6a7dc721bb37abd1f5dbe88e540f1b
oceanflow1990 <eco:ajibade@gmail.com>
2023-03-28 15:58:51 -0700

    Task 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,30 +1,20 @@
 #include "main.h"
 
 /**
- * puts2 - function to print on character out of tw
- * @str: input
+ * puts2 - print strings
+ * @str: function parameter
  * Return: 0
  */
 
 void puts2(char *str)
 {
-	int i = 0;
-	int r = 0;
-	char *w = str;
-	int o;
+	int i;
 
-	while (*w != '\0')
+	for (i = 0; str[i] != '\0' ; i++)
 	{
-		y++;
+		_putchar(str[i]);
 		i++;
 	}
-	r = i - 1;
-	for (o = 0 ; o <= r ; o++)
-	{
-		if (o % 2 == 0)
-	{
-			_putchar([o]);
-	}
-	}
+
 	_putchar('\n');
 }
