5243da69d8539514e80fcd663ddf8ef31099affe
oceanflow1990 <eco:ajibade@gmail.com>
2023-03-28 17:58:26 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* _atol - convert a string to an integer
+* _atoi - convert a string to an integer
 * @s: The pointer to convert
 * Return: A integer
 */
 
-int _atol(char *s)
+int _atoi(char *s)
 {
 int c = 0;
 unsigned int ni = 0;
