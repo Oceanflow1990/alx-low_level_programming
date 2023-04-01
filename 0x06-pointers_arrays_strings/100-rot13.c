04d71245440dafa398bd8db0286e8a93f39c51dc
oceanflow1990 <eco:ajibade@gmail.com>
2023-03-31 19:40:40 -0700

    Task 8 & 9

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,25 +1,30 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_number - print numbers chars
- * @n: integer parameterd
- * Return: 0
+ * rot13 - encoder rot13
+ * @s: pointer to string
+ *
+ * Return: *s
  */
 
-void print_number(int n)
+char *rot13(char *s)
 {
-	unsigned int n1;
+	int i;
+	int j;
+	char datal[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	n1 = n;
-
-	if (n < 0)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		_putchar('-');
-		n1 = -n;
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == datal[j])
+			{
+				s[i] = datarot[j];
+				break;
+			}
+		}
 	}
-	if (n1 / 10 != 0)
-	{
-		print_number(n1 / 10);
-	}
-	_putchar((n1 % 10) + '0');
+	return (s);
 }
