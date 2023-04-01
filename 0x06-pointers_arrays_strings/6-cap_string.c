4d27949c067a41c81f9a28775c278e005c1ec927
oceanflow1990 <eco:ajibade@gmail.com>
2023-03-31 18:16:55 -0700

    Task 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ int index = 0;
 
 while (str[index])
 {
-while (!(str[index] >= 'a' && str[index] <= '2'))
+while (!(str[index] >= 'a' && str[index] <= 'z'))
 index++;
 
 if (str[index - 1] == ' ' ||
