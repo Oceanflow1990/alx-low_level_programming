c435ba80929f3aaa006fda873c8ce69f25f1fb72
oceanflow1990 <eco:ajibade@gmail.com>
2023-04-03 13:57:00 -0700

    Task 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - function for entry point
+ * @s: input value
+ * @accept: input value
+ * Return: 0 (pass)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int j;
+
+	while (*s)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (*s == accept[j])
+			{
+				n++;
+				break;
+			}
+			else if (accept[j + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}
