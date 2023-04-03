1d5e284e582dfdd3271cea40279c90d7f67f601e
oceanflow1990 <eco:ajibade@gmail.com>
2023-04-03 14:03:32 -0700

    Task 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function for point of entry
+ * @s: value input
+ * @accept: value input
+ * Return: 0 (pass)
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int j;
+
+	while (*s)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (*s == accept[k])
+				return (s);
+		}
+		s++;
+
+	}
+	return ('\0');
+}
+
