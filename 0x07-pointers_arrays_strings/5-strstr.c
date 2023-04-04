a5d809e831558b8efcc2ec6869515db92bc88dbd
oceanflow1990 <eco:ajibade@gmail.com>
2023-04-04 16:15:54 -0700

    Task 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,3 +1,27 @@
+#include <stdio.h>
 #include "main.h"
 
+/**
+ * _strstr - function for entry level
+ * @haystack: input para
+ * @needle: input para
+ * Return: 0 (pass)
+ */
 
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *i = haystack;
+		char *j = needle;
+
+		while (*i == *j && *j != '\0')
+		{
+			i++;
+			j++;
+		}
+		if (*j == '\0')
+			return (haystack);
+	}
+	return (NULL);
+}
