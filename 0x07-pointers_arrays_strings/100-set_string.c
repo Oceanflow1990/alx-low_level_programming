5fa81b8c9a6e8e0db581f430ea7eb608533e58f8
oceanflow1990 <eco:ajibade@gmail.com>
2023-04-04 17:11:37 -0700

    task 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - Fcuntion to set string
+ * @s: input value
+ * @to: input value
+ * Return: 0 (pass)
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
