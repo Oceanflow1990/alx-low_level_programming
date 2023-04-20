0c2890e17dd6c4d07db4902e55682bb839284242
oceanflow1990 <eco:ajibade@gmail.com>
2023-04-19 20:03:10 -0700

    Task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum of two int.
+ * @a: 1st int.
+ * @b: 2cond int.
+ * Return: a + b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sum of two int.
+ * @a: 1st int.
+ * @b: 2cond int.
+ * Return: a - b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication of two int.
+ * @a: 1st int.
+ * @b: 2cond int.
+ * Return: a * b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of two int.
+ * @a: 1st int.
+ * @b: 2cond int.
+ * Return: a / b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - mod. of two int.
+ * @a: 1st int.
+ * @b: 2cond int.
+ * Return: a % b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
