9105dbfed0bb494c80a39054b56d7e562e9e6bd0
Opeyemi Ajibade <eco.ajibade@gmail.com>
2023-11-18 09:15:13 -0800

    function that returns the natural square root of a number.

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number.
+ * @n: input number
+ *
+ * Return: the square rot of a number
+ */
+int actual_sqrt_recursion(int n, int y);
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * actual_sqrt_recursion - recurses find the natural no.
+ * @n: input
+ * @y: iterator
+ *
+ * Return: square root number.
+ */
+
+int actual_sqrt_recursion(int n, int y)
+{
+	if (y * y > n)
+		return (-1);
+	if (y * y == n)
+		return (y);
+	return (actual_sqrt_recursion(n, y + 1));
+}
