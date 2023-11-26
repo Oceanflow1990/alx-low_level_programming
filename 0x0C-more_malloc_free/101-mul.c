76bfad563cb76309c9a7aaf4d17002c5435fdb4e
Opeyemi Ajibade <eco.ajibade@gmail.com>
2023-11-25 16:41:39 -0800

    ite a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,7 +5,7 @@
 #define ERR_MSG "Error"
 
 /**
- * _is_digit - check if a string contains a non-digit char
+ * is_digit - check if a string contains a non-digit char
  * @s: string to be evaluated
  *
  * Return: 0 or 1
