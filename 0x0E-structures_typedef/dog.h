16cc69c02736163ca4b659f51ba6c441c440d909
Opeyemi Ajibade <eco.ajibade@gmail.com>
2023-11-28 16:08:13 -0800

    efine a new type dog_t as a new name for the type struct dog.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,20 +6,16 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
+ *
+ * Description:- new dog
  */
 
-/**
- * dog_t - new dog name
- */
-
-typedef struct dog dog_t;
-
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+} dog_t
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
