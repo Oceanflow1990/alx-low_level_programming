2fd9f9fdc1fa0e4ecc0e1e0c292c6662c4a07d2e
Opeyemi Ajibade <eco.ajibade@gmail.com>
2023-11-30 02:29:16 -0800

    program that performs simple operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.>
+
+/**
+ * get_op_func - select the option of perform arithmentic op
+ * @s: operator
+ *
+ * Return: selection
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	/* initialize i */
+	i = 0;
+
+	/* s does not match any of the operand */
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+
+	return (ops[i].f); /* return operand */
+}
