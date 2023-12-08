0842e22015bdd2910930343ba3d3a68dfc108959
Opeyemi Ajibade <eco.ajibade@gmail.com>
2023-12-08 12:48:35 -0800

    main function is executed.

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 void first(void)__attribute__ ((constructor));
 
 /**
@@ -6,6 +7,6 @@ void first(void)__attribute__ ((constructor));
  */
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my bakc!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
